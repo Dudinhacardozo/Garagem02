c8e6e75753a126f2ca99bd2cae27b2e06bc4b031
Dudinhacardozo <dudakrdozo@gmail.com>
2024-07-01 15:05:21 -0300

    erros arrumados

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,5 +1,2 @@
 from .user import User
-
 from .acessorio import Acessorio
-from .categoria import Categoria
-from .cor import Cor
--- core/models/acessorio.py
+++ core/models/acessorio.py
@@ -4,4 +4,7 @@ class Acessorio(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.descricao
+        return f"{self.descricao} ({self.id})"
+        class Meta:
+            verbose_name = "Acessório"
+            verbose_name_plural = "acessórios"
