6bba45305f3a2c0eefa94369f88eeec33bdb23ca
Dudinhacardozo <dudakrdozo@gmail.com>
2024-07-05 16:25:23 -0300

    finalização projeto garagem

---
+++ core/models/categoria.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.descricao} ({self.id})"
+        class Meta:
+            verbose_name = "Categoria"
+            verbose_name_plural = "categorias"
---
+++ core/models/marca.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome.upper() ({self.id})
+        class Meta:
+            verbose_name = "Marca"
+            verbose_name_plural = "Marcas"
